c94496bdb0bb5b4f1352ecdf0702fc8abc3d8bc7
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-10-26 17:02:58 -0300

    adicionando styled components

---
+++ mtech-challenge/src/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Header = styled.h1`
+  color: #F00;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  span {
+    display: flex;
+    color: black;
+  }
+`;
